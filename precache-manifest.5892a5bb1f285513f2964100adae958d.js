7c0056059f7e1498bc6610ce6ab1fc2292db82ec
Ivan <snobbaspirit@gmail.com>
2019-10-03 04:49:41 +0300

    Updates

---
+++ precache-manifest.5892a5bb1f285513f2964100adae958d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ded962877294f9221e32fc8641c9bf",
+    "url": "/MnstrsRoledex/index.html"
+  },
+  {
+    "revision": "3bba6d1877181b47e253",
+    "url": "/MnstrsRoledex/static/css/main.d046cc38.chunk.css"
+  },
+  {
+    "revision": "e367aada8a0be678716b",
+    "url": "/MnstrsRoledex/static/js/2.929d243a.chunk.js"
+  },
+  {
+    "revision": "3bba6d1877181b47e253",
+    "url": "/MnstrsRoledex/static/js/main.10bf382c.chunk.js"
+  },
+  {
+    "revision": "473081762edfac271ad2",
+    "url": "/MnstrsRoledex/static/js/runtime~main.12f7dfd0.js"
+  }
+]);
